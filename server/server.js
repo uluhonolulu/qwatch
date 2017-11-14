39baa83d964aa0af420c33a4138dd7d48f9a21f2
Ulu Honolulu <uluhonolulu@gmail.com>
2017-11-14 15:15:00 +0200

    Added webhook confirmation

--- server/server.js
+++ server/server.js
@@ -9,5 +9,33 @@ app.get("/", (req, res) => {
     res.send("Hey dude!");
 } );
 
+// Adds support for GET requests to our webhook
+app.get('/webhook', (req, res) => {
+    
+      // Your verify token. Should be a random string.
+      let VERIFY_TOKEN = "from_fb";
+        
+      // Parse the query params
+      let mode = req.query['hub.mode'];
+      let token = req.query['hub.verify_token'];
+      let challenge = req.query['hub.challenge'];
+        
+      // Checks if a token and mode is in the query string of the request
+      if (mode && token) {
+      
+        // Checks the mode and token sent is correct
+        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+          
+          // Responds with the challenge token from the request
+          console.log('WEBHOOK_VERIFIED');
+          res.status(200).send(challenge);
+        
+        } else {
+          // Responds with '403 Forbidden' if verify tokens do not match
+          res.sendStatus(403);      
+        }
+      }
+    });
+
 const port = process.env.PORT || 3000;
 app.listen(port);
