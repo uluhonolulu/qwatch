8b375e9d9e6f06831c7de6dd1a3090d7f8f8acd9
Ulu Honolulu <uluhonolulu@gmail.com>
2017-11-14 12:23:31 +0200

    return smth

--- server.js
+++ server.js
@@ -3,3 +3,10 @@ const express = require('express');
 const app = express();
 
 app.listen(3000);
+
+app.get("/", (req, res) => {
+    return "Hey dude!";
+} );
+
+const port = process.env.PORT || 3000;
+app.listen(port);
