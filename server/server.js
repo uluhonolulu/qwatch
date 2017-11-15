a491a2cd4fd382707d2f62a29ed6406f8dca7332
Ulu Honolulu <uluhonolulu@gmail.com>
2017-11-15 11:13:46 +0200

    Added POST support

--- server/server.js
+++ server/server.js
@@ -1,13 +1,44 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(express.static(__dirname + '/../public' ));
+app.use(bodyParser.json())
 
 //app.listen(3000);
 console.log("Hi!");
 
 app.get("/", (req, res) => {
+    console.log("console");
     res.send("Hey dude!");
 } );
+
+// Creates the endpoint for our webhook 
+app.post('/webhook', (req, res) => {  
+    console.log("got post");
+    console.log(JSON.stringify(req.body));
+     let body = req.body;
+    
+     // Checks this is an event from a page subscription
+     if (body.object === 'page') {
+   
+       // Iterates over each entry - there may be multiple if batched
+       body.entry.forEach(function(entry) {
+   
+         // Gets the message. entry.messaging is an array, but 
+         // will only ever contain one message, so we get index 0
+         let webhookEvent = entry.messaging[0];
+         console.log(webhookEvent);
+       });
+   
+       // Returns a '200 OK' response to all requests
+       res.status(200).send('EVENT_RECEIVED');
+     } else {
+       // Returns a '404 Not Found' if event is not from a page subscription
+       res.sendStatus(404);
+     }
+   
+   });
 
 // Adds support for GET requests to our webhook
 app.get('/webhook', (req, res) => {
